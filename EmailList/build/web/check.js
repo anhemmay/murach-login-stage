10f463f31db6a859bb2584992f755f5339da26fd
giangann <70595717+giangann@users.noreply.github.com>
2022-03-11 00:30:53 +0700

    Update check.js

--- EmailList/build/web/check.js
+++ EmailList/build/web/check.js
@@ -1,33 +1,56 @@
 let err = "Not valid";
 let avaiable = "Avaiable";
+let email_status = fname_status = lname_status = false
 
 function checkInput() {
   let email = document.getElementById("email").value;
 
-  console.log(email);
   if (!email.includes("@")) {
     document.getElementById("error_mail").innerHTML = err;
+    email_status =  false
   } else {
     document.getElementById("error_mail").innerHTML = avaiable;
+    email_status =  true
   }
+  checkAllInput()
 }
 
-function checkEmpty() {
+function checkFirstName() {
   let fname = document.getElementById("firstName").value;
-  let lname = document.getElementById("lastName").value;
 
   if (isEmptyOrSpaces(fname)) {
     document.getElementById("error_fname").innerHTML = err;
+    fname_status = false
   } else {
     document.getElementById("error_fname").innerHTML = avaiable;
+    fname_status = true
   }
+  checkAllInput()
+}
 
+function checkLastName(){
+  let lname = document.getElementById("lastName").value;
   if (isEmptyOrSpaces(lname)) {
     document.getElementById("error_lname").innerHTML = err;
+    lname_status = false
   } else {
     document.getElementById("error_lname").innerHTML = avaiable;
+    lname_status = true
   }
+  checkAllInput()
 }
+
 function isEmptyOrSpaces(str) {
   return str === null || str.match(/^ *$/) !== null;
 }
+
+function checkAllInput(){
+  if (email_status && fname_status && lname_status){
+    document.getElementById("submit").disabled = false
+  } else {
+    document.getElementById("submit").disabled = true
+  }
+}
+
+
+
